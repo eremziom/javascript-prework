ccb6572a4e01790c0ecc4bb26411cee49000e861
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-07-06 14:11:07 +0200

    add all instructions & result

--- js/script.js
+++ js/script.js
@@ -1,23 +1,49 @@
 let computerMove = 'nieznany ruch';
 let playerMove = 'nieznany ruch';
-
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-console.log('Wylosowana liczba to: ' + randomNumber);
 
 if(randomNumber == 1){
     computerMove = 'kamień';
+} else if(randomNumber == 2){
+    computerMove = 'papier';
+} else if(randomNumber == 3){
+    computerMove = 'nożyce';
 }
 
 let playerInput = prompt('Twój ruch to... 1: kamień, 2: papier, 3: nożyce');
 
-console.log('Gracz wybrał: ' + playerInput);
-
 if(playerInput == '1'){
     playerMove = 'kamień';
+} else if(playerInput == '2'){
+    playerMove = 'papier';
+} else if(playerInput == '3'){
+    playerMove = 'nożyce';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+console.log('Wylosowana liczba to: ' + randomNumber);
+console.log('Gracz wybrał: ' + playerInput);
 
+printMessage('Twój ruch to: ' + playerMove);
 printMessage('Mój ruch to: ' + computerMove);
 
+if(playerMove == 'kamień' && computerMove == 'kamień'){
+    printMessage('REMIS!');
+} else if(playerMove == 'papier' && computerMove == 'papier'){
+    printMessage('REMIS!');
+} else if(playerMove == 'nożyce' && computerMove == 'nożyce'){
+    printMessage('REMIS!');
+} else if(playerMove == 'kamień' && computerMove == 'papier'){
+    printMessage('PRZEGRAŁEŚ');
+} else if(playerMove == 'papier' && computerMove == 'nożyce'){
+    printMessage('PRZEGRAŁEŚ');
+} else if(playerMove == 'nożyce' && computerMove == 'kamień'){
+    printMessage('PRZEGRAŁEŚ');
+} else if(playerMove == 'kamień' && computerMove == 'nożyce'){
+    printMessage('WYGRAŁEŚ');
+} else if(playerMove == 'papier' && computerMove == 'kamień'){
+    printMessage('WYGRAŁEŚ');
+} else if(playerMove == 'nożyce' && computerMove == 'papier'){
+    printMessage('WYGRAŁEŚ');
+} else{
+    printMessage('NIE UMIESZ WYBRAĆ POPRAWNIE LICZBY GAMONIU?');
+}
