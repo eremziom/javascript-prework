1c5493d913f2ae0b4ad3e52ca6b1b82696d3de6b
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-07-06 13:27:37 +0200

    add first instructions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let computerMove = 'nieznany ruch';
+let playerMove = 'nieznany ruch';
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+}
+
+let playerInput = prompt('Twój ruch to... 1: kamień, 2: papier, 3: nożyce');
+
+console.log('Gracz wybrał: ' + playerInput);
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+printMessage('Mój ruch to: ' + computerMove);
+
